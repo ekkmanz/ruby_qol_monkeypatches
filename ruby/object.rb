a9aad19e6c4dc78206491b7892f2d97e8b3fc179
Ekarin Supataratarn <ekarin.sup@gmail.com>
2015-12-07 11:01:12 +0700

    Add simple array coercion / destruction to Object class
    Useful when you dealing with [Array or Object] type of object e.g. XML
    results.

--- ruby/object.rb
+++ ruby/object.rb
@@ -1,5 +1,21 @@
 class Object
 
+  def marshall_clone
+    Marshal.load( Marshal.dump(self) )
+  end
+
+  def ensure_array
+    (self.is_a? Array) ? self : [self]
+  end
+
+  def first_or_self
+    (self.is_a? Array) ? first : self
+  end
+
+  def last_or_self
+    (self.is_a? Array) ? last  : self
+  end
+
   def superclasses
     return [] if self.nil?
     x = self.superclass 
